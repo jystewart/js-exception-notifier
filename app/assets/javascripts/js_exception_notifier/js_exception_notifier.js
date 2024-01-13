bc949e6465ba3d0a780eb5745499a3ac97cb7506
James Stewart (laptop) <james@jystewart.net>
2024-01-13 17:30:41 +0000

    Use fetch to submit error reports

--- app/assets/javascripts/js_exception_notifier/js_exception_notifier.js
+++ app/assets/javascripts/js_exception_notifier/js_exception_notifier.js
@@ -24,30 +24,35 @@
     return match !== undefined;
   };
 
-  JSExceptionNotifierLogger = function(errorReport) {
+  JSExceptionNotifierLogger = aync function(errorReport) {
     var ref;
     if (errorReport.message !== '' && errorReport.stack && errorReport.stack[0] && errorReport.stack[0].line > 0 && !isExcludedFile(errorReport.stack[0].url) && !isExcludedContext((ref = errorReport.stack[0].context) != null ? ref.join() : void 0)) {
       window.errorCount || (window.errorCount = 0);
       if (window.errorCount > 5) {
         return;
       }
+
       window.errorCount += 1;
       errorReport.url = document.location.href;
       errorReport.useragent = navigator.userAgent;
-      return $.ajax({
-        url: '/js_exception_notifier',
-        headers: {
-          'X-CSRF-Token': typeof jQuery === "function" ? jQuery('meta[name="csrf-token"]').attr('content') : void 0
-        },
-        data: {
-          errorReport: errorReport
-        },
-        type: 'POST',
-        dataType: 'JSON',
-        error: function(data, textStatus, jqXHR) {
-          return console.log(data);
-        }
-      });
+
+      try {
+        let response = await fetch(
+          '/js_exception_notifier', {
+            method: 'POST',
+            headers: {
+              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').attribute('content'),
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(errorReport)
+          }
+        )
+
+        return response.ok;
+      } catch (error) {
+        console.log(data);
+        return false;
+      }
     }
   }
 
